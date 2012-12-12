4b09b86bfeccf3c9f51d80a368eeeefca3a4ebff
Scott LaVigne <pyrated@gmail.com>
2012-12-11 22:44:40 -0800

    Resets allocation check

--- httpng.c
+++ httpng.c
@@ -114,7 +114,7 @@ void snap(GtkWidget* view, gpointer data) {
   gdouble scalex = (gdouble) width / (gdouble) allocation.width;
   gdouble scaley = (gdouble) height / (gdouble) allocation.height;
 
-  if (allocation.width > allocation.height)
+  if (allocation.width < allocation.height)
     cairo_scale(cr, scalex, scalex);
   else
     cairo_scale(cr, scalex, scaley);
